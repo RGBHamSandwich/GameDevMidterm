03d13942c3cc117e5a56f60439fc7ca381314cdb
Beck <rmarshall26@amherst.edu>
2025-02-17 22:42:53 +0100

    Creating and cleaning PlayerJumpScript

---
+++ PlantGame/Assets/Scripts/PlayerJumpScript.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerJump : MonoBehaviour
+{
+    ///// PUBLIC VARIABLES /////
+    [Header("Jumping")]
+    [Tooltip("How much force is applied to the player when they jump?")]
+    public float thrust = 5f;
+    [Tooltip("How long does the player have to wait before jumping again?")]
+    public float jumpWaitTime = 1f;
+    [Header("Ground Checking")]
+    [SerializeField] private Transform boxCheckPivot;
+    [SerializeField] private float boxCheckSize = 0.6f;
+    [SerializeField] private LayerMask groundLayer;
+
+    ///// PRIVATE VARIABLES /////
+    private SpriteRenderer _playerSpriteRenderer;
+    private Rigidbody2D _playerRigidbody;
+
+    ///// COROUTINES /////
+    private IEnumerator _jumpCoroutine;
+    private bool _isGrounded = true;
+
+    ///// METHODS /////
+
+    private void OnDrawGizmosSelected(){
+        if(boxCheckPivot == null) return;
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(boxCheckPivot.position, Vector3.one * boxCheckSize);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _playerSpriteRenderer = GetComponent<SpriteRenderer>();
+        _playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckIfGrounded();
+        HandleJump();
+    }
+
+    private void HandleJump()
+    {
+        if((Input.GetKeyDown(KeyCode.W) | (Input.GetKeyDown(KeyCode.Space))) && _isGrounded) 
+        {
+            _isGrounded = false;
+            StartCoroutine(JumpCoroutine(jumpWaitTime));
+            _playerRigidbody.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+            
+        }
+    }
+
+    private IEnumerator JumpCoroutine(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        _isGrounded = true;
+    }
+
+    private void CheckIfGrounded()
+    {
+        _isGrounded = Physics2D.OverlapBox(boxCheckPivot.position, Vector3.one * boxCheckSize, 0, groundLayer);
+    }
+
+}
--- PlantGame/Assets/Scripts/PlayerScript.cs
+++ PlantGame/Assets/Scripts/PlayerScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour
 {
+    ///// PUBLIC VARIABLES /////
     [Header("Movement")]
     public Sprite Neutral;
     public Sprite[] MoveUp;
@@ -10,32 +11,22 @@ public class PlayerScript : MonoBehaviour
     public Sprite[] Walk;
     public Sprite[] LayDown;
     public float framesPerSecond = 4;
-    [Tooltip("How much force is applied to the player when they jump?")]
-    public float thrust = .01f;
-    [Tooltip("How long does the player have to wait before jumping again?")]
-    public float jumpWaitTime = 1f;
     float frameTimer = 0;
     int currentFrameIndex = 0;
-
-    // recycling a lot of the code from the first game :)
     [Tooltip("How fast is the player moving left to right?")]
     public float speed = 3;
 	[Tooltip("How fast is the player able to pick up items?")]
     public float rateOfInteract = 3;
-    [Header("Ground Checking")]
-    [SerializeField] private Transform boxCheckPivot;
-    [SerializeField] private float boxCheckSize = 1;
-    [SerializeField] private LayerMask groundLayer;
+    
+    ///// PRIVATE VARIABLES /////
     private float _lastTimeInteract = 1;
-    private float _lastTimeJump = 1;
     private bool _hasPlant = false;
     private bool _layingDown = false;
     private SpriteRenderer _playerSpriteRenderer;
     private Rigidbody2D _playerRigidbody;
 
     ///// COROUTINES /////
-    private IEnumerator _jumpCoroutine;
-    private bool _isGrounded = true;
+    
 
     ///// DELEGATES /////
     public delegate void OnPlayerMovement(Vector2 movement);
@@ -45,20 +36,11 @@ public class PlayerScript : MonoBehaviour
 
     ///// METHODS /////
 
-    private void OnDrawGizmosSelected(){
-        if(boxCheckPivot == null) return;
-
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawWireCube(boxCheckPivot.position, Vector3.one * boxCheckSize);
-    }
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _playerSpriteRenderer = GetComponent<SpriteRenderer>();
         _playerRigidbody = GetComponent<Rigidbody2D>();
-
-        // StartCoroutine(_jumpCoroutine(jumpWaitTime));
     }
 
     // Update is called once per frame
@@ -66,10 +48,7 @@ public class PlayerScript : MonoBehaviour
     {
         frameTimer -= Time.deltaTime;
 
-        CheckIfGrounded();
-
         HandleMovement();
-        HandleJump();
         HandleInteract();
 
     }
@@ -81,11 +60,18 @@ public class PlayerScript : MonoBehaviour
 
         transform.position += Vector3.right * horizontal * speed * Time.deltaTime;
 
-        if(vertical > 0 && _isGrounded){  // jump as long as we haven't jumped recently
-            // _playerSpriteRenderer.sprite = MoveUp[0];
-            // transform.position += Vector3.up * vertical * speed * Time.deltaTime;
-            HandleJump();
-        } else if(vertical < 0){            // stretch goal: limit movement until player presses jump (to stand up)
+
+
+
+        // if(vertical > 0 && _isGrounded){  // jump as long as we haven't jumped recently
+        //     // _playerSpriteRenderer.sprite = MoveUp[0];
+        //     // transform.position += Vector3.up * vertical * speed * Time.deltaTime;
+        //     HandleJump();
+        // }
+
+
+
+        if(vertical < 0){            // stretch goal: limit movement until player presses jump (to stand up)
             _layingDown = true;
             // _playerSpriteRenderer.sprite = MoveDown[0];
             HandleLayingDown();
@@ -106,24 +92,23 @@ public class PlayerScript : MonoBehaviour
 
     }
 
-    private void HandleJump(){  // stolen from explosion.cs in Shootemup
-        if(Input.GetKeyDown(KeyCode.W) && _isGrounded){
-            _isGrounded = false;
-            _jumpCoroutine = JumpCoroutine(jumpWaitTime);
+    // private void HandleJump(){  // stolen from explosion.cs in Shootemup
+        // if(Input.GetKeyDown(KeyCode.W) && _isGrounded){
+            // _isGrounded = false;
+            // _jumpCoroutine = JumpCoroutine(jumpWaitTime);
 
-            _lastTimeJump = Time.time;
-            _playerRigidbody.AddForce(transform.up * thrust, ForceMode2D.Impulse);
-            if (frameTimer <= 0){
-                currentFrameIndex++;
-                if (currentFrameIndex >= MoveUp.Length){
-                    currentFrameIndex = 0;
-                    _playerSpriteRenderer.sprite = Neutral;
-                }
-                frameTimer = (1f / framesPerSecond);
-                _playerSpriteRenderer.sprite = MoveUp[currentFrameIndex];
-            }
-        }
-    }
+            // _playerRigidbody.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+        //     if (frameTimer <= 0){
+        //         currentFrameIndex++;
+        //         if (currentFrameIndex >= MoveUp.Length){
+        //             currentFrameIndex = 0;
+        //             _playerSpriteRenderer.sprite = Neutral;
+        //         }
+        //         frameTimer = (1f / framesPerSecond);
+        //         _playerSpriteRenderer.sprite = MoveUp[currentFrameIndex];
+        //     }
+        // }
+    // }
 
     private void HandleWalk() {
         if (frameTimer <= 0){
@@ -169,15 +154,6 @@ public class PlayerScript : MonoBehaviour
             // CUE INTERACTION SOUNDS (plant sounds or interact sound?)
         
         }
-    }
-
-    private void CheckIfGrounded(){
-        _isGrounded = Physics2D.OverlapBox(boxCheckPivot.position, Vector3.one * boxCheckSize, 0, groundLayer);
-    }
-
-    private IEnumerator JumpCoroutine(float waitTime){
-        yield return new WaitForSeconds(waitTime);
-        _isGrounded = true;
     }
 
 }
