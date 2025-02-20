e563b288f7b02bd12c1faec6cc5947ff914f2855
kendallparry <156971407+kendallparry@users.noreply.github.com>
2025-02-20 16:24:25 +0100

    continuing UI

--- PlantGame/Assets/Scripts/economyManager.cs
+++ PlantGame/Assets/Scripts/economyManager.cs
@@ -1,16 +1,27 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class economyManager : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+// public class economyManager : MonoBehaviour
+// {
+//     int balance;
+    
+//     public float balance(){
+//         get { return balance;};
+//         set { balance = value;};
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+//     Dictionary<string, int> shopCosts = new Dictionary<string, int>(){
+//         { "couch", 50 },
+//         { "bed", 100 },
+//         { "table", 30 },
+//         { "chair", 10 },
+//         { "rug", 20 },
+//         { "bookshelf", 30 },
+//         { "toy", 10 },
+//         { "pet", 100 },
+//         { "bedside table", 20 },
+//         { "armchair", 30 },
+//         { "wallpaper", 50 },
+//         { "painting", 40}
+//     };
+
+// }
