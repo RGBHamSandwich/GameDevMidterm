d89970d9213bd1e606dca9a73473b07e3a57a958
kendallparry <156971407+kendallparry@users.noreply.github.com>
2025-02-23 22:04:47 +0100

    added controls page!! still a wip but she's there

--- PlantGame/Assets/Scripts/PauseMenu.cs
+++ PlantGame/Assets/Scripts/PauseMenu.cs
@@ -5,6 +5,8 @@ public class PauseMenu : MonoBehaviour
     public GameObject MenuPanel;
     public GameObject PausePanel;
     public GameObject ConfirmPanel;
+
+    public GameObject ControlsPanel;
     
     void Start()
     {
@@ -12,6 +14,7 @@ public class PauseMenu : MonoBehaviour
         MenuPanel.SetActive(false);
         PausePanel.SetActive(true);
         ConfirmPanel.SetActive(false);
+        ControlsPanel.SetActive(false);
     }
 
     void Update(){
@@ -26,6 +29,7 @@ public class PauseMenu : MonoBehaviour
          MenuPanel.SetActive(true);
          PausePanel.SetActive(false);
          ConfirmPanel.SetActive(false);
+         ControlsPanel.SetActive(false);
          Time.timeScale = 0;
      }
 
@@ -47,5 +51,13 @@ public class PauseMenu : MonoBehaviour
         PausePanel.SetActive(true);
         ConfirmPanel.SetActive(false);
         Time.timeScale = 1;
+     }
+
+     public void openControls(){
+        ControlsPanel.SetActive(true);
+     }
+
+     public void closeControls(){
+        ControlsPanel.SetActive(false);
      }
 }
