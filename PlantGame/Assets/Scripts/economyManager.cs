cb3d275257ceaabc61e78318734f9a9f86cd4ad7
kendallparry <156971407+kendallparry@users.noreply.github.com>
2025-02-24 15:34:51 +0100

    starting shop menu stuff

--- PlantGame/Assets/Scripts/economyManager.cs
+++ PlantGame/Assets/Scripts/economyManager.cs
@@ -1,27 +1,46 @@
-// using UnityEngine;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
 
-// public class economyManager : MonoBehaviour
-// {
-//     int balance;
+[System.Serializable]
+public class ShopItem{
+    public GameObject furniture;
+    public int price;
+}
+
+public class economyManager : MonoBehaviour
+{
+    public ShopItem[] itemsForSale;
+
+    // public int getPrice(String item){
+    //     foreach (ShopItem i in itemsForSale){
+    //         if (i.CompareTag(item)){
+
+    //         }
+    //     }
+    // }
+
+    // int balance;
     
-//     public float balance(){
-//         get { return balance;};
-//         set { balance = value;};
-//     }
+    // // public float balance(){
+    // //     get { return balance;};
+    // //     set { balance = value;};
+    // // }
 
-//     Dictionary<string, int> shopCosts = new Dictionary<string, int>(){
-//         { "couch", 50 },
-//         { "bed", 100 },
-//         { "table", 30 },
-//         { "chair", 10 },
-//         { "rug", 20 },
-//         { "bookshelf", 30 },
-//         { "toy", 10 },
-//         { "pet", 100 },
-//         { "bedside table", 20 },
-//         { "armchair", 30 },
-//         { "wallpaper", 50 },
-//         { "painting", 40}
-//     };
+    // // Dictionary<string, int> shopCosts = new Dictionary<string, int>(){
+    // //     { "couch", 50 },
+    // //     { "bed", 100 },
+    // //     { "table", 30 },
+    // //     { "chair", 10 },
+    // //     { "rug", 20 },
+    // //     { "bookshelf", 30 },
+    // //     { "toy", 10 },
+    // //     { "pet", 100 },
+    // //     { "bedside table", 20 },
+    // //     { "armchair", 30 },
+    // //     { "wallpaper", 50 },
+    // //     { "painting", 40}
+    // // };
 
-// }
+}
