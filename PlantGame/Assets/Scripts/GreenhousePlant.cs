ff411fad0cc75f31b4897d88229136bff9d1017b
M-Brown26 <mtbrown@smith.edu>
2025-03-04 23:30:58 +0100

    the plants come back!

--- PlantGame/Assets/Scripts/GreenhousePlant.cs
+++ PlantGame/Assets/Scripts/GreenhousePlant.cs
@@ -1,22 +1,28 @@
+
+using System;
 using PlantGame.Player;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class GreenhousePlant : MonoBehaviour
 {
-    private PlayerInteractPlantScript _hasplant;
+    //private bool _hasPlant;
     private PlayerInteractPlantScript _plant;
     private GameObject player;
+    private PlayerInteractPlantScript playerScript;
     public SpriteRenderer plantRenderer;
     public GameObject plant;
+    public SpriteRenderer distanceIcon;
    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameObject player = GameObject.FindWithTag("Player");
-        SpriteRenderer plantRenderer = GetComponent<SpriteRenderer>();
+        player = GameObject.FindWithTag("Player");
+        plantRenderer = GetComponent<SpriteRenderer>();
         GameObject plant = GetComponent<GameObject>();
+        playerScript = player.GetComponent<PlayerInteractPlantScript>();
         disablePlant();
+        disableIcons();
     }
 
     // Update is called once per frame
@@ -26,12 +32,17 @@ public class GreenhousePlant : MonoBehaviour
     }
 
     void placePlant(){
-        if (player != null){
-        //if (_hasplant == true){
-            if (player.transform.position.x == plant.transform.position.x){
+        Debug.Log("hasPlant is: " + playerScript._hasPlant);
+        if (playerScript._hasPlant){
+            //if (player.transform.position.x == plant.transform.position.x){
+            if (Vector2.Distance(player.transform.position, plant.transform.position) < 1.5f){
+                distanceIcon.enabled = true;
                 if (Input.GetKey(KeyCode.E)){
+                    distanceIcon.enabled = false;
+                    Debug.Log("Pressing E");
                     plantRenderer.enabled = true;
-                    //Destroy(_plant);
+                    Destroy(_plant);
+                    //_hasplant = false;
                 }
             }
         }
@@ -41,6 +52,12 @@ public class GreenhousePlant : MonoBehaviour
         
         if (plantRenderer != null){
             plantRenderer.enabled = false;
+        }
+    }
+
+    void disableIcons(){
+        if (distanceIcon != null){
+            distanceIcon.enabled = false;
         }
     }
     
