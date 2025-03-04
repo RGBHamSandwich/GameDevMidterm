f5b2bb93df012f2f6656dd6aa156c20761e89f99
M-Brown26 <mtbrown@smith.edu>
2025-03-04 15:59:11 +0100

    created the greenhouse prefabs

--- PlantGame/Assets/Scripts/GreenhousePlant.cs
+++ PlantGame/Assets/Scripts/GreenhousePlant.cs
@@ -8,20 +8,22 @@ public class GreenhousePlant : MonoBehaviour
     private PlayerInteractPlantScript _plant;
     private GameObject player;
     public SpriteRenderer plant;
+    public GameObject plant_pos;
    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (plant != null){
-            disablePlant();
-        }
+        GameObject player = GameObject.FindWithTag("Player");
+        SpriteRenderer plant = GetComponent<SpriteRenderer>();
+        GameObject plant_pos = GetComponent<GameObject>();
+        disablePlant();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_hasplant){
-            if (player.transform.position.x == plant.transform.position.x){
+        if (_hasplant == true){
+            if (player.transform.position.x == plant_pos.transform.position.x){
                 if (Input.GetKey(KeyCode.E)){
                     plant.enabled = true;
                     Destroy(_plant);
