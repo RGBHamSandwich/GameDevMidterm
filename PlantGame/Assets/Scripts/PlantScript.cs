599399a98a418888001be1c4cacf19e2095b91c4
DESKTOP-1EKJCRT\Beck <rmarshall26@amherst.edu>
2025-03-06 23:32:33 +0100

    editing plant pickup transform

--- PlantGame/Assets/Scripts/PlantScript.cs
+++ PlantGame/Assets/Scripts/PlantScript.cs
@@ -20,15 +20,7 @@ public class PlantScript : MonoBehaviour
             float xPlayer = player.transform.position.x;
 
             float yPlant = this.transform.position.y;
-            // float xPlant = player.transform.position.x
-
-            // bound plant position with player position
-            float y = Mathf.Max(yPlant - yPlayer, yPlayer - 1f);
-            y = Mathf.Min(y, yPlayer + 1f);
-            float x = xPlayer + 0.6f;
-
-            // float yPlant = yPlayer + 0.4f;
-            // float xPlant = xPlayer - 0.6f;
+            float y = yPlant - yPlayer;
 
             transform.SetParent(player.transform);
             transform.localPosition = new Vector3(0.6f, y, 0f);
